a3929b6359c728c607ca9aee6c03998071786ccd
Ilona <ilona.atty@gmail.com>
2023-10-16 12:48:58 +0200

    Initial commit. Create React App.

---
+++ src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Note() {
+        return(
+            <div className='note'>
+            <h1>Keeper title</h1>
+            <p>Keeper content</p> 
+            </div>
+            
+        )  
+}
+
+export default Note;
