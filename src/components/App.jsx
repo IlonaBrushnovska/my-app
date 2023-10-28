9bea2bc4242fbe5c61217fc8e42c19764794d8c9
Ilona <ilona.atty@gmail.com>
2023-10-28 21:42:03 +0200

    Practicing Hooks, Object and Array Destructuring, Event Handling, React Forms

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,50 @@
-import React from "react";
-import Header from "./Header"
-import Footer from './Footer';
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
-
-function createdNotes(card) {
-    return (<Note
-        key={card.id}
-        title={card.title}
-        content={card.content}
-
-
-    />)
-}
+import CreateArea from "./CreateArea";
 
 function App() {
-    return (
-        <div>
-            <Header />
-            {notes.map(card => ( 
-                <Note
-                key={card.id}
-                title={card.title}
-                content={card.content}
-            />
-            ))}
-            <Footer />
-        </div>
-    )
+  
+//- Add new note to an array.
+  const [notes, setNotes] = useState([])
+
+  function addNote(inputNote) {
+    setNotes((prevNotes) => {
+      return ([...prevNotes, inputNote])
+    })
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      // filter out the item that needs deletion.
+      return (prevNotes.filter((obj, index) => {
+        return (index !== id)
+      }))
+    })
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onAdd={addNote} />
+
+{/* Take array and render seperate Note components for each item. */}
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onCheked={deleteNote}
+          />)
+      })}
+
+
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+function CreateArea(props) {
+
+//- Create a constant that keeps track of the title and content.
+  const [inputNote, setInputNotes] = useState({
+    title: "",
+    content: ""
+  })
+
+  function handleEvent(event) {
+   const {name, value} = event.target
+   setInputNotes(prev => { 
+    return({...prev, [name]: value})}
+    )
+  }
+
+
+  function submitNote(event) {
+    props.onAdd(note);
+    // Clear the Create Area.
+    setInputNotes({
+      title: "",
+      content: ""
+    })
+    // Prevent from reloud.
+    event.preventDefault(); 
+  }
+
+  return (
+    <div>
+      <form >
+        <input onChange={handleEvent} name="title" placeholder="Title" value={inputNote.title}/>
+        <textarea onChange={handleEvent} name="content" placeholder="Take a note..." rows="3" value={inputNote.content}/>
+        <button
+        onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
